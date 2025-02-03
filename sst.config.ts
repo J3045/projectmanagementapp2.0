1e33525c8ec0d05ac11b1080800aadcd49c92406
jainik <jainikpatel824@gmail.com>
2025-02-04 03:31:17 +0530

    setting up the deployment

---
+++ sst.config.ts
@@ -0,0 +1,29 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "projectmanagementapp",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL,
+          DIRECT_URL: process.env.DIRECT_URL,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+          AUTH_SECRET: process.env.AUTH_SECRET,
+          AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+          AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
