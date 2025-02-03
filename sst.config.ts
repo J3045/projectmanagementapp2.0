0075ac826080565019c7519a83513b129b55dd99
jainik <jainikpatel824@gmail.com>
2025-02-04 04:46:53 +0530

    setting up the deployment11

--- sst.config.ts
+++ sst.config.ts
@@ -12,12 +12,12 @@ export default {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         environment: {
-          DATABASE_URL: process.env.DATABASE_URL,
-          DIRECT_URL: process.env.DIRECT_URL,
-          NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-          AUTH_SECRET: process.env.AUTH_SECRET,
-          AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
-          AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+          DATABASE_URL: process.env.DATABASE_URL!,
+          DIRECT_URL: process.env.DIRECT_URL!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+          AUTH_SECRET: process.env.AUTH_SECRET!,
+          AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID!,
+          AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET!,
         },
       });
 
