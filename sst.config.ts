a96c98657c1dd9ed4ebb97508c87b82f4dd58684
jainik <jainikpatel824@gmail.com>
2025-02-06 01:42:34 +0530

    first commit

--- sst.config.ts
+++ sst.config.ts
@@ -1,30 +1,26 @@
-import { App, Stack } from "@serverless-stack/resources";
-import { NextjsSite } from "@serverless-stack/resources";
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-  config() {
+export default $config({
+  app(input) {
     return {
-      name: "projectmanagementapp",
-      region: "ap-south-1",
+      name: "projectmanagementapp-main",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
     };
   },
-  stacks(app: App) {
-    app.stack(function Site({ stack }: { stack: Stack }) {
-      const site = new NextjsSite(stack, "site", {
-        path: ".", // Path of your Next.js app
-        environment: {
-          NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
-          DATABASE_URL: process.env.DATABASE_URL || "",
-          AUTH_SECRET: process.env.AUTH_SECRET || "",
-          AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID || "",
-          AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET || "",
-          DIRECT_URL: process.env.DIRECT_URL || "",
-        },
-      });
-
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
+  async run() {
+    new sst.aws.Nextjs( "site", {
+      path: ".", // Path of your Next.js app
+      environment: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
+        DATABASE_URL: process.env.DATABASE_URL || "",
+        AUTH_SECRET: process.env.AUTH_SECRET || "",
+        AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID || "",
+        AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET || "",
+        DIRECT_URL: process.env.DIRECT_URL || "",
+      }
     });
   },
-};
+});
